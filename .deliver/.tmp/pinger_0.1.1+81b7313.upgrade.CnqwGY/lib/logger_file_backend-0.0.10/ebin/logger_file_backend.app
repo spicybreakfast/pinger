79e64f1acd192a013861ad19c47843cef008ca67
Travis Erard <travis@impossiblecreature.com>
2018-02-06 15:35:56 -0700

    remove padding from log message

--- .deliver/.tmp/pinger_0.1.1+81b7313.upgrade.CnqwGY/lib/logger_file_backend-0.0.10/ebin/logger_file_backend.app
+++
@@ -1,6 +0,0 @@
-{application,logger_file_backend,
-             [{description,"Simple logger backend that writes to a file"},
-              {modules,['Elixir.LoggerFileBackend']},
-              {registered,[]},
-              {vsn,"0.0.10"},
-              {applications,[kernel,stdlib,elixir]}]}.
--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :logger,
        :info,
        path: "logs/ping.log",
        level: :info,
-       format: "\n$date $time $levelpad$message"
+       format: "\n$date $time $message"
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
